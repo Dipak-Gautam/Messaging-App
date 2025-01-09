6910c843839a17da164681e621b54ecaf5556643
Dipak-Gautam <anjangautam095@gmail.com>
2025-01-09 12:47:38 +0545

    backend: route added

--- Backend/routes/requests.js
+++ Backend/routes/requests.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../modals/user");
-const { jwtAuthMiddleWare, generateJWtToken } = require("../jwt");
+const { jwtAuthMiddleWare } = require("../jwt");
+
+router.post("/", jwtAuthMiddleWare, async (req, res) => {
+  try {
+    console.log("request received");
+  } catch (error) {
+    console.log("request", error);
+    res.status(500).json({ message: "internal server error", error: error });
+  }
+});
+
+module.exports = router;
