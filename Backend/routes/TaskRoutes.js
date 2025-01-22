bb66b021a1b1a5d7935a9b144b02a76874daf23d
Dipak-Gautam <anjangautam095@gmail.com>
2025-01-22 10:58:03 +0545

    feat:task route added

--- Backend/routes/TaskRoutes.js
+++ Backend/routes/TaskRoutes.js
@@ -4,7 +4,6 @@ const User = require("../modals/user");
 const Task = require("../modals/task");
 
 router.post("/add", async (req, res) => {
-  Task;
   try {
     const data = req.body;
     const newTask = new Task(data);
@@ -12,6 +11,28 @@ router.post("/add", async (req, res) => {
     res.status(200).json("Task Added sucessfully");
   } catch (error) {
     console.log("task/add", error);
+    res.status(500).json({ message: "internal server error", error: error });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const task = await Task.find();
+    res.status(200).json(task);
+  } catch (error) {
+    console.log("task/get", error);
+    res.status(500).json({ message: "internal server error", error: error });
+  }
+});
+router.put("/status", async (req, res) => {
+  try {
+    const data = req.body;
+    const updateTask = await Task.findById(data.id);
+    updateTask.status = req.body.status;
+    const response = await updateTask.save();
+    res.status(200).json({ message: "Task Updated", data: response });
+  } catch (error) {
+    console.log("task/status update", error);
     res.status(500).json({ message: "internal server error", error: error });
   }
 });
